105f9983cfb97ecc614fb0e56a5979f6aad0c58c
adheeee <adheejr77@gmail.com>
2020-10-20 18:07:32 +0800

    add burger icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,12 +68,13 @@
       class: 'mobile-nav d-lg-none'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="fas fa-bars"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      $('.mobile-nav-toggle i').removeClass('fa-bars');
+      $('.mobile-nav-toggle i').toggleClass('fas fa-times');
       $('.mobile-nav-overly').toggle();
     });
 
@@ -88,7 +89,7 @@
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('fas fa-times');
           $('.mobile-nav-overly').fadeOut();
         }
       }
